6df9e0ce1a349002b86c253a895c6939b21a69e4
alejandro99billi <96229780+alejandro99billi@users.noreply.github.com>
2024-01-13 13:10:23 +0100

    firts commit

---
+++ connector-open-weather/src/main/java/com/aleister/connectoropenweather/ConnectorOpenWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.aleister.connectoropenweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectorOpenWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectorOpenWeatherApplication.class, args);
+	}
+
+}
---
+++ connector-open-weather/src/test/java/com/aleister/connectoropenweather/ConnectorOpenWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleister.connectoropenweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectorOpenWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
